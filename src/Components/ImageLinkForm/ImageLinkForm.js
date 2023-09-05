1ef98a8b1c6d713d60a4dccef099f3d2f6460e99
Pedro Silva <pedro.alsf94@gmail.com>
2023-09-05 15:15:46 -0300

    indicators and 2k char limit working

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -7,15 +7,20 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit, input }) => {
         {"Try it out by linking any art below:"}
         <div className="pa4 flex justify-center items-center">
           <input type="text" className="f4 w-40 mr2" onChange={onInputChange} />
-          <button
-            className="w-5 pv2 bw0 grow f6 ph3 shadow-5 white bg-mid-gray"
-            onClick={onButtonSubmit}
-          >
-            Detect
-          </button>
+          {input.length < 2000 && (
+            <button
+              className="w-5 pv2 bw0 grow f6 ph3 shadow-5 white bg-mid-gray"
+              onClick={onButtonSubmit}
+            >
+              Detect
+            </button>
+          )}
         </div>
         {input.length >= 2000 && (
-          <p className="red">Please use a link shorter than 2000 characters</p>
+          <p className="red">
+            Please use a link shorter than 2000 characters <br />
+            (open image source instead of getting link directly from google)
+          </p>
         )}
       </p>
     </div>
